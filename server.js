542eeafd31528fa979a11937a30827f3fed8b12a
Ilya Kozlov <ilya140297@gmail.com>
2018-01-18 22:37:44 +0300

    начал работу с сервером

---
+++ server.js
@@ -0,0 +1,16 @@
+const port = 3003;
+const express = require('express');
+const pug = require('pug');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './public/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.render('index.pug');
+});
+
+app.listen(port, function() {
+	console.log('listening on port ' + port);
+});
